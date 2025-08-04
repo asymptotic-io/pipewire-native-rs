a111dbd8d717f0694b12a35537c924011a57a23c
Arun Raghavan <arun@asymptotic.io>
2025-08-04 19:46:40 -0400

    pipewire: tests: Spawn a pipewire instance for our tests
    Easier to work with and debug than the global server instance.

--- pipewire/tests/lib.rs
+++ pipewire/tests/lib.rs
@@ -3,14 +3,38 @@
 // SPDX-FileCopyrightText: Copyright (c) 2025 Arun Raghavan
 
 use std::sync::Arc;
+use tempfile;
 
 use pipewire_native::{
     self as pipewire, context::Context, main_loop::MainLoop, properties::Properties,
 };
 use pipewire_native_spa::dict::Dict;
 
+#[allow(unused)]
+struct TestContext {
+    runtime_dir: tempfile::TempDir,
+    pipewire: std::process::Child,
+}
+
+fn start_pipewire() -> TestContext {
+    let runtime_dir = tempfile::tempdir().unwrap();
+
+    std::env::set_var("PIPEWIRE_RUNTIME_DIR", runtime_dir.path());
+
+    let pipewire = std::process::Command::new("pipewire").spawn().unwrap();
+
+    std::thread::sleep(std::time::Duration::from_secs(2));
+
+    TestContext {
+        runtime_dir,
+        pipewire,
+    }
+}
+
 #[test]
 fn test_lib() {
+    let _test_context = start_pipewire();
+
     pipewire::init();
 
     let v: Vec<(String, String)> = vec![("loop.name".to_string(), "pw-main-loop".to_string())];
