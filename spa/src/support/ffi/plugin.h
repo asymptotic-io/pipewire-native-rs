40e42fa53c38c243cc9ae49ecc1c63b3f2a20ad3
Arun Raghavan <arun@asymptotic.io>
2025-08-25 19:41:15 -0400

    spa: ffi: Add a missing copyright header

--- spa/src/support/ffi/plugin.h
+++ spa/src/support/ffi/plugin.h
@@ -1,3 +1,7 @@
+// SPDX-License-Identifier: MIT
+// SPDX-FileCopyrightText: Copyright (c) 2025 Asymptotic Inc.
+// SPDX-FileCopyrightText: Copyright (c) 2025 Arun Raghavan
+
 #include <stdint.h>
 
 struct c_callbacks {
